21820aa9fdc55fefdf86ed990b75166a9337fd3f
Farley Nunes <84993015+FarNunes@users.noreply.github.com>
2021-11-03 11:31:21 -0300

    Update consultaClima.py

--- consultaClima.py
+++ consultaClima.py
@@ -1,11 +1,16 @@
+'''
+Para fazer consultas do clima em tempo real.
+Para esse code, você precisara da API Key para setar sua localização e com isso ter o clima da sua região.
+Para esse caso utilizei o API da Open Weather Map.
+'''
+
 import requests
 import json
 
-# Api key: f82d01a916d7f21a797a32f5169b98ec // 9c16bc51194b4575d0da080ef765264d
 
 cidade = input('Informe sua cidade: ')
 
-requisicao = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=9c16bc51194b4575d0da080ef765264d')
+requisicao = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+cidade+'(Coloque aqui a sua API Key)')
 
 tempo = json.loads(requisicao.text)
 
