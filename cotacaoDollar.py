66cb57fe2d1caf3a4274772abb4a9d00086c6f67
Farley Nunes <84993015+FarNunes@users.noreply.github.com>
2021-11-03 11:17:55 -0300

    Add files via upload

---
+++ cotacaoDollar.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import time
+import datetime
+
+while True:
+    requisicao = requests.get('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL')
+    cotacao = json.loads(requisicao.text)
+
+    print('### Cotação ###', datetime.datetime.now())
+    print('Dolar:', cotacao['USDBRL']['high'])
+    print('Variação do Dolar:', cotacao['USDBRL']['varBid'])
+    print('Euro:', cotacao['EURBRL']['high'])
+    print('Variação do Euro:', cotacao['EURBRL']['varBid'])
+    print('BitCoin:', cotacao['BTCBRL']['high'])
+    print('Variação do BitCoin:', cotacao['BTCBRL']['varBid'])
+    time.sleep(30)
